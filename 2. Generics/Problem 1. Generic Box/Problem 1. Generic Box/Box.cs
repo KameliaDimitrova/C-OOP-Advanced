9d8b2b3690e75015833ca635974cded9b640e20d
baskam <baskam@students.softuni.bg>
2018-03-26 15:38:36 +0300

    Add new task

---
+++ 2. Generics/Problem 1. Generic Box/Problem 1. Generic Box/Box.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+public class Box<T>
+{
+    private T value;
+
+    public Box(T value)
+    {
+        this.value = value;
+    }
+    public Box()
+    { }
+    public string SwapTwoElements(List<T> listForSwapping, int firstIndex, int secondIndex)
+    {
+        var firstElement = listForSwapping[firstIndex];
+        var secondElement = listForSwapping[secondIndex];
+        listForSwapping[firstIndex] = secondElement;
+        listForSwapping[secondIndex] = firstElement;
+        var sb = new StringBuilder();
+        foreach (var element in listForSwapping)
+        {
+            sb.AppendLine($"{element.GetType().FullName}: {element}");
+        }
+        return sb.ToString().Trim();
+    }
+
+    public override string ToString()
+    {
+        var result = $"{this.value.GetType().FullName}: {value}";
+        return result;
+    }
+}
+
---
+++ 2. Generics/Problem 1. Generic Box/Problem 1. Generic Box/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1._Generic_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stringList = new List<string>();
+            var box=new Box<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = (Console.ReadLine());
+                stringList.Add(input);
+            }
+            var indexes = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            var firstIndex = int.Parse(indexes[0]);
+            var secondIndex = int.Parse(indexes[1]);
+
+            Console.WriteLine(box.SwapTwoElements(stringList, firstIndex, secondIndex));
+
+        }
+    }
+}
