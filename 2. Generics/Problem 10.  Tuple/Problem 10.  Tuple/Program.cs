2542a8e1ac4e0c5dadc7d3b91a3afa18cf2eb4ff
baskam <baskam@students.softuni.bg>
2018-03-27 17:16:29 +0300

    Add tupple task

---
+++ 2. Generics/Problem 10.  Tuple/Problem 10.  Tuple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_10.__Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var inputFirstData = Console.ReadLine()
+                .Split(' ');
+            var name = inputFirstData[0] +" "+ inputFirstData[1];
+            var addres = inputFirstData[2];
+            Tuple<string,string> tupple1=new Tuple<string, string>(name,addres);
+            Console.WriteLine(tupple1);
+            var inputSecondData = Console.ReadLine()
+                .Split(' ');
+            var firstItem = inputSecondData[0];
+            var secondItem = int.Parse(inputSecondData[1]);
+            Tuple<string, int> tupple2 = new Tuple<string, int>(firstItem, secondItem);
+            Console.WriteLine(tupple2);
+            var inputThirdData = Console.ReadLine()
+                .Split(' ');
+            var firstItem3 = int.Parse(inputThirdData[0]);
+            var secondItem3 = double.Parse(inputThirdData[1]);
+            Tuple<int, double> tupple3 = new Tuple<int, double>(firstItem3, secondItem3);
+            Console.WriteLine(tupple3);
+
+        }
+    }
+}
---
+++ 2. Generics/Problem 10.  Tuple/Problem 10.  Tuple/Tuple.cs
@@ -0,0 +1,14 @@
+﻿public class Tuple<TKey1,TKey2>
+{
+    public TKey1 Key1 { get; private set; }
+    public TKey2 Key2 { get; private set; }
+    public Tuple(TKey1 key1,TKey2 key2)
+    {
+        this.Key1 = key1;
+        this.Key2 = key2;
+    }
+    public override string ToString()
+    {
+        return $"{Key1} -> {Key2}";
+    }
+}
