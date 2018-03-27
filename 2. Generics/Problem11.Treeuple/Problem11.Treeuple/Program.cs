8e00d9237e438247077aec4971ead7780e37a89f
baskam <baskam@students.softuni.bg>
2018-03-27 17:32:14 +0300

    Add new treeuple task

---
+++ 2. Generics/Problem11.Treeuple/Problem11.Treeuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem11.Treeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFirstData = Console.ReadLine()
+                .Split(' ');
+            var name = inputFirstData[0] + " " + inputFirstData[1];
+            var addres = inputFirstData[2];
+            var town = inputFirstData[3];
+            Treeuple<string, string,string> treeupple1 = new Treeuple<string, string,string>(name, addres,town);
+            Console.WriteLine(treeupple1);
+            var inputSecondData = Console.ReadLine()
+                .Split(' ');
+            var firstItem = inputSecondData[0];
+            var secondItem = int.Parse(inputSecondData[1]);
+            var thirdItem = "False";
+            if (inputSecondData[2] == "drunk")
+            {
+                thirdItem = "True";
+            }
+           Treeuple<string, int,string> treeuple2= new Treeuple<string, int,string>(firstItem, secondItem,thirdItem);
+            Console.WriteLine(treeuple2);
+            var inputThirdData = Console.ReadLine()
+                .Split(' ');
+            var firstItem3 = (inputThirdData[0]);
+            var secondItem3 = double.Parse(inputThirdData[1]);
+            var thirdItem3 = inputThirdData[2];
+            Treeuple<string, double,string> treeuple3 = new Treeuple<string, double,string>(firstItem3, secondItem3,thirdItem3);
+            Console.WriteLine(treeuple3);
+        }
+    }
+}
---
+++ 2. Generics/Problem11.Treeuple/Problem11.Treeuple/Treeuple.cs
@@ -0,0 +1,19 @@
+﻿public class Treeuple<TKey1,TKey2,TKey3>
+{
+    public TKey1 Key1 { get;  }
+    public TKey2 Key2 { get;  }
+    public TKey3 Key3 { get;  }
+
+    public Treeuple(TKey1 key1,TKey2 key2, TKey3 key3)
+    {
+        this.Key1 = key1;
+        this.Key2 = key2;
+        this.Key3 = key3;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Key1} -> {this.Key2} -> {this.Key3}";
+    }
+}
+
