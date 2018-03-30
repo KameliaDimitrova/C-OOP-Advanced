5c1b274393b0229378c09422890e6ccfc1141f8d
baskam <baskam@students.softuni.bg>
2018-03-30 14:25:48 +0300

    Add new task

---
+++ 2. Generics/Problem1.ListyIterator/Problem1.ListyIterator/ListyIterator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public  class ListyIterator<T>:IEnumerable<T>
+{
+    private readonly List<T> collection;
+    private int index;
+
+    public ListyIterator(List<T> collection)
+    {
+        this.collection = collection;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.collection.Count; i++)
+        {
+            yield return this.collection[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Move()
+    {
+        if (index++ < collection.Count)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string  Print()
+    {
+        if (this.collection.Count == 0)
+            return "Invalid Operation!";
+        return collection[index].ToString();
+    }
+
+    public string PrintAll()
+    {
+        return string.Join(' ', this.collection);
+    }
+
+    public bool HasNext()
+    {
+        var currIndex = this.index+1;
+        if (currIndex < collection.Count)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ 2. Generics/Problem1.ListyIterator/Problem1.ListyIterator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<string>();
+            ListyIterator<string> listyIterator=new ListyIterator<string>(collection);
+            var input = Console.ReadLine()
+                .Split(' ');
+            var arguments = input.Skip(1).ToList();
+            if (arguments.Count > 0)
+            {
+                collection = arguments;
+                listyIterator = new ListyIterator<string>(collection);
+            }
+            var command = Console.ReadLine();
+            while (command != "END")
+            {
+                
+                switch (command)
+                {
+                   case "Print":
+                        Console.WriteLine(listyIterator.Print());
+                        break;
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(listyIterator.PrintAll());
+                        break;
+                    default: break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
