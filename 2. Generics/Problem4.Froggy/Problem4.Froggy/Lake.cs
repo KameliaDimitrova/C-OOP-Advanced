49a1b64fd7fcbd102cc76ca6cc72b66959a5a166
baskam <baskam@students.softuni.bg>
2018-03-30 16:05:31 +0300

    Add new task

---
+++ 2. Generics/Problem4.Froggy/Problem4.Froggy/Lake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class Lake<T>:IEnumerable<T>
+{
+    private List<T> collection;
+
+    public Lake(List<T> collection)
+    {
+        this.collection = new List<T>(collection);
+    }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Count; i+=2)
+            {
+                yield return collection[i];
+            }
+        for (int i = this.collection.Count - 1; i > 0; i--)
+        {
+            if (i % 2 == 0)
+            {
+                continue;
+            }
+            yield return this.collection[i];
+        }
+    }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+}
+
---
+++ 2. Generics/Problem4.Froggy/Problem4.Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem4.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+            Lake<int> lake=new Lake<int>(input);
+            Console.WriteLine(string.Join(", ", lake));
+
+        }
+    }
+}
