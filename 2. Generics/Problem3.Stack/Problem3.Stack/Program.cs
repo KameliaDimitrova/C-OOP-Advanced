ba5009660011be550835d75d19fd758fc73c362f
baskam <baskam@students.softuni.bg>
2018-03-30 15:38:38 +0300

    add new task

---
+++ 2. Generics/Problem3.Stack/Problem3.Stack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem3.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new List<int>();
+            Stack<int> stack=new Stack<int>(collection);
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var commandArgs = input
+                    .Split(' ')
+                    .ToList();
+                var command = commandArgs[0];
+               collection = input.Split(new []{',',' '}, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+                    switch (command)
+                {
+                    case "Push":
+                        stack.Push(collection);
+                        break;
+                    case "Pop":
+                        if(stack.Pop()!=string.Empty)
+                            Console.WriteLine(stack.Pop());
+                        break;
+                    default: break;
+                }
+                input = Console.ReadLine();
+            }
+            if (stack.Count()!=0)
+            {
+                Console.WriteLine(stack);
+
+            }
+
+        }
+    }
+}
---
+++ 2. Generics/Problem3.Stack/Problem3.Stack/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+public class Stack<T> : IEnumerable<T>
+{
+    private  List<T> stack;
+
+    public Stack(List<T> collection)
+    {
+        this.stack = new List<T>();
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < stack.Count; i++)
+            yield return stack[i];
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public string Pop()
+    {
+        if (stack.Count == 0)
+        {
+            return "No elements";
+        }
+        stack.RemoveAt(stack.Count-1);
+        return "";
+    }
+
+    public void Push(List<T> elements)
+    {
+      foreach (var element in elements)
+        {
+            stack.Add(element);
+        }
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        var printCounter = 1;
+        while (printCounter < 3)
+        {
+            for(int i=stack.Count-1;i>=0;i--)
+            {
+                sb.AppendLine(stack[i].ToString());
+            }
+            printCounter++;
+        }
+        return sb.ToString().Trim();
+    }
+}
+
