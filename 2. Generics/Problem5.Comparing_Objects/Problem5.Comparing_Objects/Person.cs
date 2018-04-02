80efe19291f2caccfbd90398abaa125fb5dc40d5
baskam <baskam@students.softuni.bg>
2018-04-02 13:52:19 +0300

    Add new homework task

---
+++ 2. Generics/Problem5.Comparing_Objects/Problem5.Comparing_Objects/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public  class Person:IComparable<Person>
+    {
+        public string Name { get;  set; }
+        public int Age { get;private set; }
+        public string Town { get; private set; }
+
+        public Person(string name, int age,string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public int CompareTo(Person otherPerson)
+        {
+            if (this.Name.CompareTo(otherPerson.Name) == 0
+            &&this.Age.CompareTo(otherPerson.Age)==0
+            &&this.Town.CompareTo(otherPerson.Town)==0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+    }
+
---
+++ 2. Generics/Problem5.Comparing_Objects/Problem5.Comparing_Objects/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem5.Comparing_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, Person> persons = new Dictionary<int, Person>();
+            var input = Console.ReadLine();
+            var listOfEqualsPersons=new List<int>();
+            int position = 1;
+            while (input != "END")
+            {
+                var inputArgs = input
+                    .Split(' ')
+                    .ToList();
+                var name = inputArgs[0];
+                var age = int.Parse(inputArgs[1]);
+                var town = inputArgs[2];
+                var person = new Person(name, age, town);
+                persons[position] = person;
+                position++;
+                input = Console.ReadLine();
+            }
+            int positionToCompare = int.Parse(Console.ReadLine());
+            foreach (var person in persons)
+            {
+                if (person.Key != positionToCompare)
+                {
+                    if (persons[positionToCompare].CompareTo(person.Value) == 0)
+                    {
+                        listOfEqualsPersons.Add(person.Key);
+                    }
+                }
+            }
+            if (listOfEqualsPersons.Count > 0)
+            {
+                Console.WriteLine($"{listOfEqualsPersons.Count+1} {persons.Count-1-listOfEqualsPersons.Count} {persons.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
