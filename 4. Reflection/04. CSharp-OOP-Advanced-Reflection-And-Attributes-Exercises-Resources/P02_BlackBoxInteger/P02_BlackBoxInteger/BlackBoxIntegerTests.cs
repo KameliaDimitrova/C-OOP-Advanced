848841b205111f8a6c11f6863a09ddb185080198
baskam <baskam@students.softuni.bg>
2018-04-04 14:52:19 +0300

    Add new homework task

--- 4. Reflection/04. CSharp-OOP-Advanced-Reflection-And-Attributes-Exercises-Resources/P02_BlackBoxInteger/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
+++ 4. Reflection/04. CSharp-OOP-Advanced-Reflection-And-Attributes-Exercises-Resources/P02_BlackBoxInteger/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -1,4 +1,6 @@
-﻿namespace P02_BlackBoxInteger
+﻿using System.Reflection;
+
+namespace P02_BlackBoxInteger
 {
     using System;
 
@@ -6,7 +8,27 @@
     {
         public static void Main()
         {
-            //TODO put your reflection code here
+            Type type = typeof(BlackBoxInteger);
+            var blackBoxInteger = (BlackBoxInteger)Activator.CreateInstance(type, true);
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                var inputArgs = input.Split('_');
+                var methodName = inputArgs[0];
+                var methodParam = int.Parse(inputArgs[1]);
+                var wantedMethod = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+                if (wantedMethod != null)
+                {
+                    wantedMethod.Invoke(blackBoxInteger, new object[] {methodParam});
+                    var fieldName = "innerValue";
+                    var innerFieldForPrint = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+                    Console.WriteLine(innerFieldForPrint.GetValue(blackBoxInteger));
+
+                }
+                input =Console.ReadLine();
+            }
+
         }
     }
 }
+
